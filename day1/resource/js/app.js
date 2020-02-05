abdb01bf4c770c44d661ead60bc253ef9a210250
baekCode <sosaer88@gmail.com>
2020-02-05 22:40:30 +0900

    add, 메인 리뷰 carousel 구현 준비 - currunt index 값 구하기

---
+++ day1/resource/js/app.js
@@ -0,0 +1,45 @@
+const ELEMENTS = {
+    wrap: '.review-list',
+    item: '.review-card',
+    prev: '.btn-prev',
+    next: '.btn-next'
+};
+const OPTION = {
+    initIndex: 0,
+    curruntIndex: 0,
+    margin: 20,
+    inteval: 5000,
+    speed: 500
+};
+const wrap = document.querySelector(ELEMENTS.wrap);
+const item = document.querySelector(ELEMENTS.item);
+const itemWidth = document.querySelector(ELEMENTS.item).offsetWidth;
+const itemLength = document.querySelectorAll(ELEMENTS.item).length;
+const itemMove = itemWidth + OPTION.margin;
+
+wrap.style.position = 'relative';
+
+const nextIndex = () => {
+    if (OPTION.curruntIndex === itemLength - 1)
+        return (OPTION.curruntIndex = OPTION.initIndex);
+    return OPTION.curruntIndex++;
+};
+const prevIndex = () => {
+    if (OPTION.curruntIndex === OPTION.initIndex) {
+        OPTION.curruntIndex = itemLength;
+        return OPTION.curruntIndex--;
+    }
+    return OPTION.curruntIndex--;
+};
+const prev = () => {
+    prevIndex();
+    console.log(OPTION.curruntIndex);
+    wrap.style.left = -(itemMove * OPTION.curruntIndex) + 'px';
+};
+const next = () => {
+    nextIndex();
+    console.log(OPTION.curruntIndex);
+    wrap.style.left = -(itemMove * OPTION.curruntIndex) + 'px';
+};
+document.querySelector(ELEMENTS.prev).addEventListener('click', prev);
+document.querySelector(ELEMENTS.next).addEventListener('click', next);
