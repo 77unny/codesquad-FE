e46a02fa77b914b32e068d440cd1c833359b3d2c
baekCode <sosaer88@gmail.com>
2020-02-21 15:15:20 +0900

    modify, app.js 변경

---
+++ day3/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'hbs');
+
+const apiServer = 'http://localhost:8081/';
+
+app.get('/', (req, res) => {
+    fetch(apiServer)
+        .then(res => res.text())
+        .then(body => {
+            res.render('index', {
+                title: '아마존 Card UI 구현',
+                subject: '아마존 card ui 구현하기 - node express ',
+                cardNav: JSON.parse(body).navData,
+                cardContents: JSON.parse(body).contentsData,
+                cardBtns: JSON.parse(body).btns
+            });
+        });
+}); 
+
+app.listen(8080);
