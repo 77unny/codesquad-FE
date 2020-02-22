63e4147d778f7073bf50cb59bc9715496d5fa15a
baekCode <sosaer88@gmail.com>
2020-02-22 21:42:55 +0900

    modify, index.hbs 에서 script

---
+++ day3/public/js/index.js
@@ -0,0 +1,1 @@
+const {Slider} = require('./slider')
---
+++ day3/public/js/slider.js
@@ -0,0 +1,113 @@
+class Slider {
+    constructor(option) {
+        (this.INIT_INDEX = 0),
+            (this.CURRUNT_INDEX = option.view),
+            (this.SPEED = option.speed),
+            (this.VIEW = option.view);
+        this.wrapEl = document.querySelector(option.wrapEl);
+        this.navEl = document.querySelector(option.navEl);
+        this.navLiEl = this.navEl.querySelectorAll('li');
+        this.contentsEl = document.querySelector(option.contentsEl);
+        this.itemsEl = document.querySelectorAll(option.itemsEl);
+        this.itemEl = document.querySelector(option.itemEl);
+        this.prevEl = document.querySelector(option.prevEl);
+        this.nextEl = document.querySelector(option.nextEl);
+        this.run();
+    }
+    initStyle() {
+        const CLONE_NUMBER = 2;
+        this.wrapEl.style.overflow = 'hidden';
+        this.wrapEl.style.position = 'relative';
+        this.wrapEl.style.width = this.itemEl.offsetWidth * this.VIEW + 'px';
+        this.contentsEl.style.position = 'relative';
+        this.contentsEl.style.width =
+            this.itemEl.offsetWidth *
+                (this.itemsEl.length + this.VIEW * CLONE_NUMBER) +
+            'px';
+        this.contentsEl.style.transform =
+            'translateX(' + -this.itemEl.offsetWidth * this.VIEW + 'px)';
+    }
+    cloneEl() {
+        const count = this.VIEW;
+        for (let i = 0; i < count; i++) {
+            const firstChild = this.itemsEl[i];
+            const lastChild = this.itemsEl[this.itemsEl.length - (i + 1)];
+            const firstCloned = firstChild.cloneNode(true);
+            const lastCloned = lastChild.cloneNode(true);
+            this.contentsEl.appendChild(firstCloned).classList.add('cloned');
+            this.contentsEl
+                .insertBefore(lastCloned, this.contentsEl.firstElementChild)
+                .classList.add('cloned');
+        }
+    }
+    onMove() {
+        this.contentsEl.style.transform = `translateX(-${this.itemEl
+            .offsetWidth * this.CURRUNT_INDEX}px)`;
+        this.onNavIndex();
+    }
+    onNextClick() {
+        this.nextEl.addEventListener('click', () => {
+            if (this.CURRUNT_INDEX === this.itemsEl.length + this.VIEW) return;
+            this.contentsEl.addEventListener('transitionend', () => {
+                if (this.CURRUNT_INDEX === this.itemsEl.length + this.VIEW) {
+                    this.contentsEl.style.transition = 'none';
+                    this.CURRUNT_INDEX = this.VIEW;
+                    this.onMove();
+                }
+            });
+            this.contentsEl.style.transition = this.SPEED + 'ms';
+            this.CURRUNT_INDEX++;
+            this.onMove();
+        });
+    }
+    onPrevClick() {
+        this.prevEl.addEventListener('click', () => {
+            if (this.CURRUNT_INDEX === this.INIT_INDEX) return;
+            this.contentsEl.addEventListener('transitionend', () => {
+                if (this.CURRUNT_INDEX === this.INIT_INDEX) {
+                    this.contentsEl.style.transition = 'none';
+                    this.CURRUNT_INDEX = this.itemsEl.length;
+                    this.onMove();
+                }
+            });
+            this.contentsEl.style.transition = this.SPEED + 'ms';
+            this.CURRUNT_INDEX--;
+            this.onMove();
+        });
+    }
+    onNavClick() {
+        const INIT_ADD_NUMBER = 1;
+        for (const iterator of this.navLiEl) {
+            iterator.addEventListener('click', () => {
+                this.CURRUNT_INDEX =
+                    parseInt(iterator.dataset.item) + INIT_ADD_NUMBER;
+                this.contentsEl.style.transition = this.SPEED + 'ms';
+                this.onMove();
+            });
+        }
+    }
+    onNavIndex() {
+        console.log('NAV INDEX => ', this.CURRUNT_INDEX);
+        console.log(this.navLiEl[this.CURRUNT_INDEX - 1])
+    }
+    run() {
+        this.initStyle();
+        this.cloneEl();
+        this.onNavClick();
+        this.onNextClick();
+        this.onPrevClick();
+    }
+}
+const slider = new Slider({
+    wrapEl: '#card',
+    navEl: '.card-nav',
+    contentsEl: '.card-contents',
+    itemsEl: '.card-item',
+    itemEl: '.card-item',
+    prevEl: '.btn-prev',
+    nextEl: '.btn-next',
+    speed: 500,
+    view: 1
+});
+
+localStorage.setItem('test','test')
