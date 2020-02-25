5049e191feae655bed85f9d587bf63f89bd16281
baekCode <sosaer88@gmail.com>
2020-02-25 19:53:00 +0900

    refact, 검색기능 리패토링 - 파라미터를 받아 함수형으로 구현

--- day4_search/search.js
+++ day4_search/search.js
@@ -1,16 +1,19 @@
 const word = ['apple', 'hello', 'world', 'appstore'];
 const search = document.querySelector('#search input');
 
-const inputValue = e => {
-    const value = e.target.value;
-    const result = [];
-    word.forEach(str => {
-        if (str.slice(0, value.length) === value) {
-            result.push(str);
-        }
-    });
-    console.log(result);
-    return result;
+const searchList = (el,arr) => {
+    const inputValue = e => {
+        const value = e.target.value;
+        const result = [];
+        arr.forEach(str => {
+            if (str.slice(0, value.length) === value) {
+                result.push(str);
+            }
+        });
+        console.log(result);
+        return result;
+    };
+    
+    el.addEventListener('input', inputValue)
 };
-
-search.addEventListener('input', inputValue);
+searchList(search,word);
