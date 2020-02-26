5849ff2a2e56ec5bd8960fa414228c51aa1e4e97
baekCode <sosaer88@gmail.com>
2020-02-26 20:08:42 +0900

    add, 네이밍변경 및 검색 키워드 리스트 기능 추가 - 네이밍 추후 변경예정 (어울리지않음) - 검색키워드 리스트추가, 해당검색 갯수 만큼 볼드 처리

--- day4_search/search.js
+++ day4_search/search.js
@@ -1,22 +1,33 @@
 const word = ['apple', 'hello', 'world', 'appstore'];
-const search = document.querySelector('#search input');
+const $$ = target => document.querySelector(target);
+const search = $$('#search');
+const searchInput = $$('#search input');
 
-const searchList = (el,arr) => {
-    const createEl = document.createElement('div')
-    document.body.appendChild(createEl);
+const searchList = (el, arr) => {
+    const createEl = document.createElement('div');
+    createEl.classList.add('keyword-list');
+    search.appendChild(createEl);
     const inputValue = e => {
         const value = e.target.value;
-        const result = [];
-        if(value !== ''){
+        const keywordList = [];
+        if (value !== '') {
+            createEl.classList.add('open');
             arr.forEach(str => {
                 if (str.slice(0, value.length) === value) {
-                    result.push(str);
+                    keywordList.push(str);
                 }
             });
-        };
-        return createEl.innerHTML = result;
+        } else {
+            createEl.classList.remove('open');
+        }
+        const result = keywordList.reduce((acc, cur) => {
+            return (acc += `<div><strong>${cur.slice(
+                0,
+                value.length
+            )}</strong>${cur.slice(value.length, cur.length)}</div>`);
+        }, '');
+        return (createEl.innerHTML = result);
     };
-
-    el.addEventListener('input', inputValue)
+    el.addEventListener('input', inputValue);
 };
-searchList(search,word);
+searchList(searchInput, word);
