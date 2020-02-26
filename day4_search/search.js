04a12f7d51f7a4130bd7e97b0367c79390f5d8be
baekCode <sosaer88@gmail.com>
2020-02-26 09:41:19 +0900

    modify, 입력조건에 대한 출력 처리 - app이라고 치면 출력은 apple, appstore - apple 라고 치면 출력은 apple만 출력

--- day4_search/search.js
+++ day4_search/search.js
@@ -2,18 +2,20 @@ const word = ['apple', 'hello', 'world', 'appstore'];
 const search = document.querySelector('#search input');
 
 const searchList = (el,arr) => {
+    const createEl = document.createElement('div')
+    document.body.appendChild(createEl);
     const inputValue = e => {
         const value = e.target.value;
         const result = [];
+        
         arr.forEach(str => {
             if (str.slice(0, value.length) === value) {
                 result.push(str);
             }
         });
-        console.log(result);
-        return result;
+        return createEl.innerHTML = result;
     };
-    
+
     el.addEventListener('input', inputValue)
 };
 searchList(search,word);
